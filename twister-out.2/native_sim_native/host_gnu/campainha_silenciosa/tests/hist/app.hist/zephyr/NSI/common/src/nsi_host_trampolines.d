6f3535eebd38d2d3c722b498544c103bb7324d69
Momesso <momessoalves@gmail.com>
2026-07-06 15:06:14 -0300

    Código final.

--- twister-out.2/native_sim_native/host_gnu/campainha_silenciosa/tests/hist/app.hist/zephyr/NSI/common/src/nsi_host_trampolines.d
+++
@@ -1,2 +0,0 @@
-/home/momesso/zephyrproject/campainha_silenciosa/twister-out/native_sim_native/host_gnu/campainha_silenciosa/tests/hist/app.hist/zephyr/NSI/common/src/nsi_host_trampolines.o: \
- /home/momesso/zephyrproject/zephyr/scripts/native_simulator//common/src/nsi_host_trampolines.c
--- twister-out.3/native_sim_native/host_gnu/campainha_silenciosa/tests/hist/app.hist/zephyr/NSI/linker_script.d
+++
@@ -1,2 +0,0 @@
-linker_script.pre.o: \
- /home/momesso/zephyrproject/zephyr/scripts/native_simulator//common/other/linker_script.pre.ld
